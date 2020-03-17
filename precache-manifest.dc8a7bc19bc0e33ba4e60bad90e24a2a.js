8ba16233b0faed232da510178cbec8f0bc93a274
Allen Jang <e1668058@cmaisonneuve.qc.ca>
2020-03-16 21:12:29 -0400

    updated version with htaccess

---
+++ precache-manifest.dc8a7bc19bc0e33ba4e60bad90e24a2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc7d1299fe1fa3b54380ce65ada4fc7",
+    "url": "/slbuild/index.html"
+  },
+  {
+    "revision": "1dea9ee86ff4f6704795",
+    "url": "/slbuild/static/css/main.1d9d2b35.chunk.css"
+  },
+  {
+    "revision": "5e5a6e451cc783145bef",
+    "url": "/slbuild/static/js/2.09ccb745.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/slbuild/static/js/2.09ccb745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dea9ee86ff4f6704795",
+    "url": "/slbuild/static/js/main.7e943848.chunk.js"
+  },
+  {
+    "revision": "d4fa5adb8d0457be2e46",
+    "url": "/slbuild/static/js/runtime-main.8c30acc5.js"
+  }
+]);
